14837f5ba83acfb2fc9824a6b5d50a19bab37220
Christo Fogelberg <cgf@syntilect.com>
2015-06-05 22:00:08 +0100

    add tasks to todo list

--- src/components/TodoList.react.js
+++ src/components/TodoList.react.js
@@ -5,12 +5,30 @@ var React = require('react');
 var TodoItem = require('./TodoItem.react');
 
 var TodoList = React.createClass({
+  getInitialState: function() {
+    return {
+      items: this.props.items
+    };
+  },
+
   render: function() {
-    var items = this.props.items.map(function(item) {
-      return <TodoItem item={item} />;
+    var items = this.props.items.map(function(item, index) {
+      return <TodoItem key={index} item={item} />;
     })
 
-    return <ol>{items}</ol>;
+    return <div>
+      <ol>{items}</ol>
+      <input id="todo-item" type="text" />
+      <button onClick={this._onClickAddItem}>Add item</button>
+    </div>;
+  },
+
+  _onClickAddItem: function(event) {
+    var new_task_text = document.getElementById('todo-item').value;
+    document.getElementById('todo-item').value = '';
+    var new_items = this.state.items;
+    new_items.push(new_task_text);
+    this.setState({ items: new_items });
   }
 });
 
